ead1c3799b8f6eba14f47fa5fb49d0ce5bfcdcba
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2020-05-23 13:26:25 +0200

    compliance_tests: Implement 7.8.1.2

--- compliance_tests/TestIso_7_8_1_2.cpp
+++ compliance_tests/TestIso_7_8_1_2.cpp
@@ -12,16 +12,16 @@
 
 /******************************************************************************
  * 
- * @test ISO16845 7.8.1.1
+ * @test ISO16845 7.8.1.2
  * 
  * @brief The purpose of this test is to verify the position of the sample
- *        point of an IUT on bit position BRS.
+ *        point of an IUT on bit position DATA field.
  * 
  * @version Classical CAN, CAN FD Tolerant, CAN FD Enabled
  * 
  * Test variables:
- *   Sampling_Point(N) and SJW(N) configuration as available by IUT.
- *      BRS
+ *      Sampling_Point(D) configuration as available by IUT.
+ *      DATA field
  *      FDF = 1
  * 
  * Elementary test cases:
@@ -31,7 +31,7 @@
  *             before sampling point;
  *          #2 test for late sampling point: bit level change to recessive
  *             after sampling point.
- *
+ *          
  *      Refer to 6.2.3.
  *
  * Setup:
@@ -39,21 +39,22 @@
  * 
  * Execution:
  *  The LT sends a frame according to elementary test cases.
- *      Test BRS #1:
- *          The LT forces the BRS bit to dominant from beginning up to one
- *          TQ(N) before Sampling_Point(N).
- *      Test BRS #2:
- *          The LT forces the BRS bit to dominant from beginning up to
- *          Sampling_Point(N).
+ * 
+ *  Test Data #1:
+ *      The LT forces a recessive bit to dominant from beginning up to one TQ(D)
+ *      before the sampling point.
+ * 
+ *  Test DATA #2:
+ *      The LT forces a dominant bit to recessive for Phase_Seg2(D).
  * 
  * Response:
- *  Test BRS #1:
- *      The modified BRS bit shall be sampled as recessive.
+ *  Test DATA #1:
+ *      The modified data bit shall be sampled as recessive.
  *      The frame is valid. No error flag shall occur.
- *  Test BRS #2:
- *      The modified BRS bit shall be sampled as dominant.
- *      The frame is valid. No error flag shall occur. The bit rate will not
- *      switch for data phase.
+ * 
+ *  Test DATA #2:
+ *      The modified data bit shall be sampled as dominant.
+ *      The frame is valid. No error flag shall occur.
  *****************************************************************************/
 
 #include <iostream>
@@ -78,7 +79,7 @@
 
 using namespace can;
 
-class TestIso_7_8_1_1 : public test_lib::TestBase
+class TestIso_7_8_1_2 : public test_lib::TestBase
 {
     public:
 
@@ -88,28 +89,35 @@ class TestIso_7_8_1_1 : public test_lib::TestBase
             TestBase::run();
             testMessage("Test %s : Run Entered", testName);
 
+            // To avoid stuff bits in data field.
+            uint8_t dataByteRecessiveSampled = 0x55;
+            uint8_t dataByteDominantSampled = 0x15;
+
             /*****************************************************************
              * BRS sampled Recessive (Shift) / BRS sample dominant (no shift)
              ****************************************************************/
 
             for (int i = 0; i < 2; i++)
             {
-                // CAN FD frame, Shift/ No shift based on iteration!
-                FrameFlags frameFlags;
-                if (i == 0)
-                    frameFlags = FrameFlags(CAN_FD, BIT_RATE_SHIFT);
-                else
-                    frameFlags = FrameFlags(CAN_FD, BIT_RATE_DONT_SHIFT);
+                // CAN FD frame, shift bit rate
+                FrameFlags frameFlags = FrameFlags(CAN_FD, BIT_RATE_SHIFT);
 
-                goldenFrame = new Frame(frameFlags);
+                // In 2nd iteration dominant bit will be sampled at second bit
+                // position of data field! We must expect this in golden frame
+                // so that it will be compared correctly with received frame!
+                if (i == 0)
+                    goldenFrame = new Frame(frameFlags, 1, &dataByteRecessiveSampled);
+                else
+                    goldenFrame = new Frame(frameFlags, 1, &dataByteDominantSampled);
+
                 goldenFrame->randomize();
                 testBigMessage("Test frame:");
                 goldenFrame->print();
 
                 if (i == 0)
-                    testMessage("Testing BRS sampled Recessive");
+                    testMessage("Testing Data bit sampled Recessive");
                 else
-                    testMessage("Testing BRS sampled Dominant");
+                    testMessage("Testing Data bit sampled Dominant");
 
                 // Convert to Bit frames
                 driverBitFrame = new BitFrame(*goldenFrame,
@@ -120,29 +128,24 @@ class TestIso_7_8_1_1 : public test_lib::TestBase
                 /**
                  * Modify test frames:
                  *   1. Turn monitor frame as if received!
-                 *   2. Flip bit value to be sure that forced value has an
-                 *      effect!
-                 *   3. Force TSEG1 - 1 of BRS to dominant (i == 0), or TSEG1
-                 *      of BRS to dominant (i == 1).
+                 *   2. Modify 2nd bit of data field. Since data is 0x55
+                 *      this bit is recessive. Flip its TSEG - 1 (i == 0)
+                 *      or TSEG1 (i == 1) to dominant.
                  */
                 monitorBitFrame->turnReceivedFrame();
                 driverBitFrame->getBitOf(0, BIT_TYPE_ACK)->setBitValue(DOMINANT);
 
-                Bit *brsBit = driverBitFrame->getBitOf(0, BIT_TYPE_BRS);
-
-                // For both set the orig. bit value to recessive so that we
-                // see the dominant flipped bits!
-                brsBit->setBitValue(RECESSIVE);
+                Bit *dataBit = driverBitFrame->getBitOf(1, BIT_TYPE_DATA);
+                dataBit->setBitValue(RECESSIVE);
 
                 int domPulseLength;
-                
                 if (i == 0)
-                    domPulseLength = nominalBitTiming.prop + nominalBitTiming.ph1;
+                    domPulseLength = dataBitTiming.prop + dataBitTiming.ph1;
                 else
-                    domPulseLength = nominalBitTiming.prop + nominalBitTiming.ph1 + 1;
+                    domPulseLength = dataBitTiming.prop + dataBitTiming.ph1 + 1;
 
                 for (int j = 0; j < domPulseLength; j++)
-                    brsBit->forceTimeQuanta(j, DOMINANT);
+                    dataBit->forceTimeQuanta(j, DOMINANT);    
 
                 driverBitFrame->print(true);
                 monitorBitFrame->print(true);
--- test_lib/TestLoader.cpp
+++ test_lib/TestLoader.cpp
@@ -80,6 +80,7 @@
 #include "../compliance_tests/TestIso_7_7_11.cpp"
 
 #include "../compliance_tests/TestIso_7_8_1_1.cpp"
+#include "../compliance_tests/TestIso_7_8_1_2.cpp"
 
 
 /******************************************************************************
@@ -208,7 +209,8 @@ test_lib::TestBase* constructTestObject(std::string name)
 
     } else if (name == "iso_7_8_1_1") {
         testPtr = new TestIso_7_8_1_1();
-
+    } else if (name == "iso_7_8_1_2") {
+        testPtr = new TestIso_7_8_1_2();
 
     } else {
         std::cerr << "Unknown test name: " << name << std::endl;
