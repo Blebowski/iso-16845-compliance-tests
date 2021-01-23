4dd792db84e7dac4282e5c793acde8233b931edc
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2021-01-23 14:19:45 +0100

    compliance_tests: Next refactor

--- compliance_tests/TestIso_7_7_2.cpp
+++ compliance_tests/TestIso_7_7_2.cpp
@@ -76,105 +76,95 @@ class TestIso_7_7_2 : public test_lib::TestBase
             for (size_t i = 0; i < nominal_bit_timing.GetBitLengthTimeQuanta(); i++){
                 ElementaryTest test = ElementaryTest(i + 1, FrameType::Can2_0);
                 test.e = i + 1;
-                elem_tests[0].push_back(test);
+                AddElemTest(TestVariant::Common, std::move(test));
             }
 
             //CanAgentSetMonitorInputDelay(std::chrono::nanoseconds(0));
             CanAgentConfigureTxToRxFeedback(true);
         }
 
-        int Run()
+        DISABLE_UNUSED_ARGS
+
+        int RunElemTest(const ElementaryTest &elem_test, const TestVariant &test_variant)
         {
-            SetupTestEnvironment();
+            frame_flags = std::make_unique<FrameFlags>(elem_test.frame_type, IdentifierType::Base);
 
-            for (size_t test_variant = 0; test_variant < test_variants.size(); test_variant++)
-            {
-                PrintVariantInfo(test_variants[test_variant]);
+            /* Base ID = 0x0 */
+            golden_frm = std::make_unique<Frame>(*frame_flags, 0x1, 0x0);
+            RandomizeAndPrint(golden_frm.get());
 
-                for (auto elem_test : elem_tests[test_variant])
-                {
-                    PrintElemTestInfo(elem_test);
+            driver_bit_frm = ConvertBitFrame(*golden_frm);
+            monitor_bit_frm = ConvertBitFrame(*golden_frm);
 
-                    frame_flags = std::make_unique<FrameFlags>(elem_test.frame_type,
-                                                               IdentifierType::Base);
+            driver_bit_frm_2 = ConvertBitFrame(*golden_frm);
+            monitor_bit_frm_2 = ConvertBitFrame(*golden_frm);
 
-                    /* Base ID = 0x0 */
-                    golden_frm = std::make_unique<Frame>(*frame_flags, 0x1, 0x0);
-                    RandomizeAndPrint(golden_frm.get());
+            /**************************************************************************************
+             * Modify test frames:
+             *   1. Monitor frame as if received.
+             *   2. Flip 5-th bit of Base identifier to dominant. This should cause error frame to
+             *      be transmitted from next bit
+             *   3. Insert Active Error frame to monitored frame. Insert Passive Error frame to
+             *      driven frame.
+             *   4. Shorten last bit of intermission by NTQ - e. This leaves third bit of
+             *      intermission with lenght of only e.
+             *   5. Turn second monitored frame as if received.
+             *   6. In second frame, force 5-th bit of Base identifier to dominant.
+             *   7. In second frame, shorten SOF by 1 TQ (this corresponds to hard-sync with end
+             *      of SYNC phase).
+             *   8. In second frame, insert active error frame from 6-th bit of Base Identifier to
+             *      monitored frame. Insert Passive error frame to driven frame.
+             *   9. Append second frame to first frame.
+             *************************************************************************************/
+            monitor_bit_frm->TurnReceivedFrame();
 
-                    driver_bit_frm = ConvertBitFrame(*golden_frm);
-                    monitor_bit_frm = ConvertBitFrame(*golden_frm);
+            driver_bit_frm->GetBitOf(4, BitType::BaseIdentifier)->FlipBitValue();
 
-                    driver_bit_frm_2 = ConvertBitFrame(*golden_frm);
-                    monitor_bit_frm_2 = ConvertBitFrame(*golden_frm);
+            driver_bit_frm->InsertPassiveErrorFrame(5, BitType::BaseIdentifier);
+            monitor_bit_frm->InsertActiveErrorFrame(5, BitType::BaseIdentifier);
 
-                    /******************************************************************************
-                     * Modify test frames:
-                     *   1. Monitor frame as if received.
-                     *   2. Flip 5-th bit of Base identifier to dominant. This should cause error
-                     *      frame to be transmitted from next bit
-                     *   3. Insert Active Error frame to monitored frame. Insert Passive Error
-                     *      frame to driven frame.
-                     *   4. Shorten last bit of intermission by NTQ - e. This leaves third bit
-                     *      of intermission with lenght of only e.
-                     *   5. Turn second monitored frame as if received.
-                     *   6. In second frame, force 5-th bit of Base identifier to dominant.
-                     *   7. In second frame, shorten SOF by 1 TQ (this corresponds to hard-sync
-                     *      with end of SYNC phase).
-                     *   8. In second frame, insert active error frame from 6-th bit of Base
-                     *      Identifier to monitored frame. Insert Passive error frame to driven
-                     *      frame.
-                     *   9. Append second frame to first frame.
-                     *****************************************************************************/
-                    monitor_bit_frm->TurnReceivedFrame();
+            /* Clear all phases and keep only SYNC. Then lengthen SYNC accordingly! */
+            Bit *last_interm_bit_drv = driver_bit_frm->GetBitOf(2, BitType::Intermission);
+            Bit *last_interm_bit_mon = monitor_bit_frm->GetBitOf(2, BitType::Intermission);
 
-                    driver_bit_frm->GetBitOf(4, BitType::BaseIdentifier)->FlipBitValue();
+            last_interm_bit_drv->ShortenPhase(BitPhase::Ph2, nominal_bit_timing.ph2_);
+            last_interm_bit_drv->ShortenPhase(BitPhase::Ph1, nominal_bit_timing.ph1_);
+            last_interm_bit_drv->ShortenPhase(BitPhase::Prop, nominal_bit_timing.prop_);
 
-                    driver_bit_frm->InsertPassiveErrorFrame(5, BitType::BaseIdentifier);
-                    monitor_bit_frm->InsertActiveErrorFrame(5, BitType::BaseIdentifier);
+            last_interm_bit_mon->ShortenPhase(BitPhase::Ph2, nominal_bit_timing.ph2_);
+            last_interm_bit_mon->ShortenPhase(BitPhase::Ph1, nominal_bit_timing.ph1_);
+            last_interm_bit_mon->ShortenPhase(BitPhase::Prop, nominal_bit_timing.prop_);
 
-                    /* Clear all phases and keep only SYNC. Then lengthen SYNC accordingly! */
-                    Bit *last_interm_bit_drv = driver_bit_frm->GetBitOf(2, BitType::Intermission);
-                    Bit *last_interm_bit_mon = monitor_bit_frm->GetBitOf(2, BitType::Intermission);
+            last_interm_bit_drv->LengthenPhase(BitPhase::Sync, elem_test.e - 1);
+            last_interm_bit_mon->LengthenPhase(BitPhase::Sync, elem_test.e - 1);
 
-                    last_interm_bit_drv->ShortenPhase(BitPhase::Ph2, nominal_bit_timing.ph2_);
-                    last_interm_bit_drv->ShortenPhase(BitPhase::Ph1, nominal_bit_timing.ph1_);
-                    last_interm_bit_drv->ShortenPhase(BitPhase::Prop, nominal_bit_timing.prop_);
+            monitor_bit_frm_2->TurnReceivedFrame();
 
-                    last_interm_bit_mon->ShortenPhase(BitPhase::Ph2, nominal_bit_timing.ph2_);
-                    last_interm_bit_mon->ShortenPhase(BitPhase::Ph1, nominal_bit_timing.ph1_);
-                    last_interm_bit_mon->ShortenPhase(BitPhase::Prop, nominal_bit_timing.prop_);
+            driver_bit_frm_2->GetBitOf(4, BitType::BaseIdentifier)->FlipBitValue();
 
-                    last_interm_bit_drv->LengthenPhase(BitPhase::Sync, elem_test.e - 1);
-                    last_interm_bit_mon->LengthenPhase(BitPhase::Sync, elem_test.e - 1);
+            driver_bit_frm_2->GetBitOf(0, BitType::Sof)->ShortenPhase(BitPhase::Sync, 1);
+            monitor_bit_frm_2->GetBitOf(0, BitType::Sof)->ShortenPhase(BitPhase::Sync, 1);
 
-                    monitor_bit_frm_2->TurnReceivedFrame();
+            driver_bit_frm_2->InsertPassiveErrorFrame(5, BitType::BaseIdentifier);
+            monitor_bit_frm_2->InsertActiveErrorFrame(5, BitType::BaseIdentifier);
 
-                    driver_bit_frm_2->GetBitOf(4, BitType::BaseIdentifier)->FlipBitValue();
+            driver_bit_frm->AppendBitFrame(driver_bit_frm_2.get());
+            monitor_bit_frm->AppendBitFrame(monitor_bit_frm_2.get());
 
-                    driver_bit_frm_2->GetBitOf(0, BitType::Sof)->ShortenPhase(BitPhase::Sync, 1);
-                    monitor_bit_frm_2->GetBitOf(0, BitType::Sof)->ShortenPhase(BitPhase::Sync, 1);
+            driver_bit_frm->Print(true);
+            monitor_bit_frm->Print(true);
 
-                    driver_bit_frm_2->InsertPassiveErrorFrame(5, BitType::BaseIdentifier);
-                    monitor_bit_frm_2->InsertActiveErrorFrame(5, BitType::BaseIdentifier);
+            /***************************************************************************** 
+             * Execute test
+             *****************************************************************************/
+            dut_ifc->SetRec(0);
+            PushFramesToLowerTester(*driver_bit_frm, *monitor_bit_frm);
+            RunLowerTester(true, true);
+            CheckLowerTesterResult();
+            /* No frame shall be received by IUT since both had errors in it! */
+            CheckNoRxFrame();
 
-                    driver_bit_frm->AppendBitFrame(driver_bit_frm_2.get());
-                    monitor_bit_frm->AppendBitFrame(monitor_bit_frm_2.get());
-
-                    driver_bit_frm->Print(true);
-                    monitor_bit_frm->Print(true);
-
-                    /***************************************************************************** 
-                     * Execute test
-                     *****************************************************************************/
-                    dut_ifc->SetRec(0);
-                    PushFramesToLowerTester(*driver_bit_frm, *monitor_bit_frm);
-                    RunLowerTester(true, true);
-                    CheckLowerTesterResult();
-                    /* No frame shall be received by IUT since both had errors in it! */
-                    CheckNoRxFrame();
-                }
-            }
-            return (int)FinishTest();
+            return FinishElementaryTest();
         }
+        ENABLE_UNUSED_ARGS
 };
