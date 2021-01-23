f729645b60c845e6d5885ac3f5c13e4ba1554589
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2021-01-23 12:16:22 +0100

    compliance_tests: Refactor 7.2.4

--- compliance_tests/TestIso_7_2_4.cpp
+++ compliance_tests/TestIso_7_2_4.cpp
@@ -82,143 +82,138 @@ class TestIso_7_2_4 : public test_lib::TestBase
         {
             FillTestVariants(VariantMatchingType::CanFdEnabledOnly);
             for (int i = 0; i < 8; i++)
-                    elem_tests[0].push_back(ElementaryTest(i + 1));
+                AddElemTest(TestVariant::CanFdEnabled, ElementaryTest(i + 1));
 
             CanAgentConfigureTxToRxFeedback(true);
         }
 
-        int Run()
+        DISABLE_UNUSED_ARGS
+
+        int RunElemTest(const ElementaryTest &elem_test, const TestVariant &test_variant)
         {
-            SetupTestEnvironment();
+            uint8_t data[64] = {};
 
-            for (size_t test_variant = 0; test_variant < test_variants.size(); test_variant++)
+            switch (elem_test.index)
             {
-                PrintVariantInfo(test_variants[test_variant]);
+            case 1:
+                data[0] = 0x10;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0x78;
+                break;
 
-                /** In this test there is no strict matching of elementary test. ISO states that
-                 *  each test shall test each stuff bit and a number. Following counter shall
-                 *  keep track of how many stuff bits were tested per variant
-                 */
-                int num_variants_tested = 0;
+            case 2:
+                data[0] = 0x78;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0x3C;
+                break;
 
-                for (auto elem_test : elem_tests[test_variant])
-                {
-                    PrintElemTestInfo(elem_test);
+            case 3:
+                data[0] = 0x34;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0x1E;
+                break;
 
-                    uint8_t data[64] = {};
+            case 4:
+                data[0] = 0x12;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0x0F;
+                break;
 
-                    switch (elem_test.index)
-                    {
-                    case 1:
-                        data[0] = 0x10;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0x78;
-                        break;
+            case 5:
+                data[0] = 0x0F;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0x87;
+                break;
 
-                    case 2:
-                        data[0] = 0x78;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0x3C;
-                        break;
+            case 6:
+                data[0] = 0x17;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0xC3;
+                break;
 
-                    case 3:
-                        data[0] = 0x34;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0x1E;
-                        break;
+            case 7:
+                data[0] = 0x43;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0xE1;
+                break;
 
-                    case 4:
-                        data[0] = 0x12;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0x0F;
-                        break;
+            case 8:
+                data[0] = 0x21;
+                for (int i = 1; i < 64; i++)
+                    data[i] = 0xF0;
+                break;
 
-                    case 5:
-                        data[0] = 0x0F;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0x87;
-                        break;
-
-                    case 6:
-                        data[0] = 0x17;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0xC3;
-                        break;
-
-                    case 7:
-                        data[0] = 0x43;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0xE1;
-                        break;
-
-                    case 8:
-                        data[0] = 0x21;
-                        for (int i = 1; i < 64; i++)
-                            data[i] = 0xF0;
-                        break;
-
-                    default:
-                        break;
-                    }
-                    
-                    frame_flags = std::make_unique<FrameFlags>(FrameType::CanFd,
-                                                    IdentifierType::Base, RtrFlag::DataFrame,
-                                                    BrsFlag::Shift, EsiFlag::ErrorActive);
-                    golden_frm = std::make_unique<Frame>(*frame_flags, 0xF, 0x555, data);
-                    RandomizeAndPrint(golden_frm.get());
-
-                    driver_bit_frm = ConvertBitFrame(*golden_frm);
-                    monitor_bit_frm = ConvertBitFrame(*golden_frm);
-
-                    /**********************************************************************************
-                     * Modify test frames:
-                     *   1. Turn monitored frame to received.
-                     *   2. Pick one of the stuff bits within the frame and flip its value.
-                     *   3. Insert Active Error frame to monitored frame. Insert Passive Error frame
-                     *      to driven frame (TX/RX feedback enabled).
-                     **********************************************************************************/
-                    monitor_bit_frm->TurnReceivedFrame();
-
-                    int num_stuff_bits = driver_bit_frm->GetNumStuffBits(StuffBitType::NormalStuffBit);
-
-                    /*****************************************************************************
-                     * Execute test
-                     ****************************************************************************/
-                    for (int stuff_bit = 0; stuff_bit < num_stuff_bits; stuff_bit++)
-                    {
-                        TestMessage("Testing stuff bit nr: %d", stuff_bit);
-                        printf("%d\n", stuff_bit);
-                        num_variants_tested++;
-
-                        /* 
-                         * Copy frame to second frame so that we dont loose modification of bits.
-                         * Corrupt only second one.
-                         */
-                        driver_bit_frm_2 = std::make_unique<BitFrame>(*driver_bit_frm);
-                        monitor_bit_frm_2 = std::make_unique<BitFrame>(*monitor_bit_frm);
-
-                        Bit *stuff_bit_to_flip = driver_bit_frm_2->GetStuffBit(stuff_bit);
-                        int bit_index = driver_bit_frm_2->GetBitIndex(stuff_bit_to_flip);
-                        stuff_bit_to_flip->FlipBitValue();
-
-                        driver_bit_frm_2->InsertPassiveErrorFrame(bit_index + 1);
-                        monitor_bit_frm_2->InsertActiveErrorFrame(bit_index + 1);
-
-                        /* Do the test itself */
-                        dut_ifc->SetRec(0);
-                        PushFramesToLowerTester(*driver_bit_frm_2, *monitor_bit_frm_2);
-                        RunLowerTester(true, true);
-                        CheckLowerTesterResult();
-
-                        driver_bit_frm_2.reset();
-                        monitor_bit_frm_2.reset();
-                    }
-                    FreeTestObjects();
-                }
-                TestBigMessage("Tested %d stuff bits in this variant!", num_variants_tested);
-                printf("%d\n", num_variants_tested);
+            default:
+                break;
             }
+            
+            frame_flags = std::make_unique<FrameFlags>(FrameType::CanFd, IdentifierType::Base,
+                                    RtrFlag::DataFrame, BrsFlag::Shift, EsiFlag::ErrorActive);
+            golden_frm = std::make_unique<Frame>(*frame_flags, 0xF, 0x555, data);
+            RandomizeAndPrint(golden_frm.get());
 
-            return (int)FinishTest();
+            driver_bit_frm = ConvertBitFrame(*golden_frm);
+            monitor_bit_frm = ConvertBitFrame(*golden_frm);
+
+            /**************************************************************************************
+             * Modify test frames:
+             *   1. Turn monitored frame to received.
+             *   2. Pick one of the stuff bits within the frame and flip its value.
+             *   3. Insert Active Error frame to monitored frame. Insert Passive Error frame
+             *      to driven frame (TX/RX feedback enabled).
+             *************************************************************************************/
+            monitor_bit_frm->TurnReceivedFrame();
+
+            int num_stuff_bits = driver_bit_frm->GetNumStuffBits(StuffBitType::NormalStuffBit);
+
+            /* In FD enabled variant, if last bit of data field is stuff bit, but model has this bit
+             * as fixed stuff bit before Stuff count. So count in also each fixed stuff bit even
+             * if last bit of data is NOT regular stuff bit. Then total number of stuff bits within
+             * FD enabled variant will be higher than in ISO 16845, but this does not mind!
+             */
+            Bit *bit = driver_bit_frm->GetBitOf(0, BitType::StuffCount);
+            int index = driver_bit_frm->GetBitIndex(bit);
+            BitValue value = driver_bit_frm->GetBit(index - 1)->bit_value_;
+            if ((value == driver_bit_frm->GetBit(index - 2)->bit_value_) &&
+                (value == driver_bit_frm->GetBit(index - 3)->bit_value_) &&
+                (value == driver_bit_frm->GetBit(index - 4)->bit_value_) &&
+                (value == driver_bit_frm->GetBit(index - 5)->bit_value_))
+                num_stuff_bits++;
+
+            /**************************************************************************************
+             * Execute test
+             *************************************************************************************/
+            for (int stuff_bit = 0; stuff_bit < num_stuff_bits; stuff_bit++)
+            {
+                TestMessage("Testing stuff bit nr: %d", stuff_bit);
+                stuff_bits_in_variant++;
+
+                /* 
+                 * Copy frame to second frame so that we dont loose modification of bits.
+                 * Corrupt only second one.
+                 */
+                driver_bit_frm_2 = std::make_unique<BitFrame>(*driver_bit_frm);
+                monitor_bit_frm_2 = std::make_unique<BitFrame>(*monitor_bit_frm);
+
+                Bit *stuff_bit_to_flip = driver_bit_frm_2->GetStuffBit(stuff_bit);
+                int bit_index = driver_bit_frm_2->GetBitIndex(stuff_bit_to_flip);
+                stuff_bit_to_flip->FlipBitValue();
+
+                driver_bit_frm_2->InsertPassiveErrorFrame(bit_index + 1);
+                monitor_bit_frm_2->InsertActiveErrorFrame(bit_index + 1);
+
+                /* Do the test itself */
+                dut_ifc->SetRec(0);
+                PushFramesToLowerTester(*driver_bit_frm_2, *monitor_bit_frm_2);
+                RunLowerTester(true, true);
+                CheckLowerTesterResult();
+
+                driver_bit_frm_2.reset();
+                monitor_bit_frm_2.reset();
+            }
+            FreeTestObjects();
+
+            return FinishElementaryTest();
         }
+        ENABLE_UNUSED_ARGS
 };
