4f7b1d572ea31345bddf0a0554e714009e407904
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2021-02-12 22:53:09 +0100

    compliance_tests: Add different sample points within bit-rate.

--- compliance_tests/TestIso_8_7_6.cpp
+++ compliance_tests/TestIso_8_7_6.cpp
@@ -75,75 +75,100 @@ using namespace test_lib;
 class TestIso_8_7_6 : public test_lib::TestBase
 {
     public:
+        BitTiming test_nom_bit_timing;
 
         void ConfigureTest()
         {
             FillTestVariants(VariantMatchingType::Common);
-            elem_tests[0].push_back(ElementaryTest(1, FrameType::Can2_0));
+
+            // Elementary test for each possible positon of sample point, restrict to shortest
+            // possible PROP = 2, shortest possible PH2 = 1. Together we test TQ(N) - 2 tests!
+            for (size_t i = 1; i < nominal_bit_timing.GetBitLengthTimeQuanta() - 2; i++)
+                AddElemTest(TestVariant::Common, ElementaryTest(i + 1, FrameType::Can2_0));
+
             CanAgentMonitorSetTrigger(CanAgentMonitorTrigger::TxFalling);
             CanAgentSetWaitForMonitor(true);
         }
 
-        int Run()
+        DISABLE_UNUSED_ARGS
+
+        int RunElemTest(const ElementaryTest &elem_test, const TestVariant &test_variant)
         {
-            SetupTestEnvironment();
+            // Calculate new bit-rate from configured one. Have same bit-rate, but
+            // different sample point. Shift sample point from 2 TQ up to 1 TQ before the
+            // end.
+            test_nom_bit_timing.brp_ = nominal_bit_timing.brp_;
+            test_nom_bit_timing.sjw_ = nominal_bit_timing.sjw_;
+            test_nom_bit_timing.ph1_ = 0;
+            test_nom_bit_timing.prop_ = elem_test.index;
+            test_nom_bit_timing.ph2_ = nominal_bit_timing.GetBitLengthTimeQuanta() - elem_test.index - 1;
+            
+            /* Re-configure bit-timing for this test so that frames are generated with it! */
+            this->nominal_bit_timing = test_nom_bit_timing;
 
-            for (size_t test_variant = 0; test_variant < test_variants.size(); test_variant++)
-            {
-                PrintVariantInfo(test_variants[test_variant]);
+            // Reconfigure DUT with new Bit time config with same bit-rate but other SP.
+            dut_ifc->Disable();
+            dut_ifc->ConfigureBitTiming(test_nom_bit_timing, data_bit_timing);
+            dut_ifc->Enable();
+            TestMessage("Waiting till DUT is error active!");
+            while (this->dut_ifc->GetErrorState() != FaultConfinementState::ErrorActive)
+                usleep(100000);
 
-                for (auto elem_test : elem_tests[test_variant])
-                {
-                    PrintElemTestInfo(elem_test);
+            TestMessage("Nominal bit timing for this elementary test:");
+            test_nom_bit_timing.Print();
 
-                    frame_flags = std::make_unique<FrameFlags>(FrameType::Can2_0,
-                                                               EsiFlag::ErrorActive);
-                    golden_frm = std::make_unique<Frame>(*frame_flags);
-                    RandomizeAndPrint(golden_frm.get());
+            frame_flags = std::make_unique<FrameFlags>(FrameType::Can2_0, EsiFlag::ErrorActive);
+            golden_frm = std::make_unique<Frame>(*frame_flags);
+            RandomizeAndPrint(golden_frm.get());
 
-                    driver_bit_frm = ConvertBitFrame(*golden_frm);
-                    monitor_bit_frm = ConvertBitFrame(*golden_frm);
+            driver_bit_frm = ConvertBitFrame(*golden_frm);
+            monitor_bit_frm = ConvertBitFrame(*golden_frm);
 
-                    /******************************************************************************
-                     * Modify test frames:
-                     *   1. Set Ack dominant in driven frame.
-                     *   2. Choose random recessive bit which is followed by dominant bit.
-                     *   3. Shorten chosen bit by 1 TQ in driven and monitored frames. This
-                     *      corresponds to resynchronization by 1 TQ.
-                     *   4. Force 2nd Time quanta of dominant bit after the random recessive bit
-                     *      to Recessive in driven frame.
-                     *****************************************************************************/
-                    driver_bit_frm->GetBitOf(0, BitType::Ack)->bit_value_ = BitValue::Dominant;
+            /******************************************************************************
+             * Modify test frames:
+             *   1. Set Ack dominant in driven frame.
+             *   2. Choose random recessive bit which is followed by dominant bit.
+             *   3. Shorten chosen bit by 1 TQ in driven and monitored frames. This
+             *      corresponds to resynchronization by 1 TQ.
+             *   4. Force 2nd Time quanta of dominant bit after the random recessive bit
+             *      to Recessive in driven frame.
+             * 
+             *   Note: The check that next edge shall be sent integer number of time
+             *         quantas after it is executed by the fact that we don not do any
+             *         further resynchronizations. Model takes care of it then!
+             *****************************************************************************/
+            driver_bit_frm->GetBitOf(0, BitType::Ack)->bit_value_ = BitValue::Dominant;
 
-                    Bit *rand_bit;
-                    Bit *next_bit;
-                    size_t rand_bit_index;
-                    do {
-                        rand_bit = driver_bit_frm->GetRandomBit(BitValue::Recessive);
-                        rand_bit_index = driver_bit_frm->GetBitIndex(rand_bit);
-                        if (rand_bit_index == driver_bit_frm->GetBitCount() - 1)
-                            continue;
-                        next_bit = driver_bit_frm->GetBit(rand_bit_index + 1);
-                    } while (next_bit == nullptr || next_bit->bit_value_ == BitValue::Recessive);
+            Bit *rand_bit = nullptr;
+            Bit *next_bit = nullptr;
+            size_t rand_bit_index;
+            do {
+                rand_bit = driver_bit_frm->GetRandomBit(BitValue::Recessive);
+                rand_bit_index = driver_bit_frm->GetBitIndex(rand_bit);
+                if (rand_bit_index == driver_bit_frm->GetBitCount() - 1)
+                    continue;
+                next_bit = driver_bit_frm->GetBit(rand_bit_index + 1);
+            } while (next_bit == nullptr || next_bit->bit_value_ == BitValue::Recessive);
 
-                    rand_bit->ShortenPhase(BitPhase::Ph2, 1);
-                    monitor_bit_frm->GetBit(rand_bit_index)->ShortenPhase(BitPhase::Ph2, 1);
+            rand_bit->ShortenPhase(BitPhase::Ph2, 1);
+            monitor_bit_frm->GetBit(rand_bit_index)->ShortenPhase(BitPhase::Ph2, 1);
 
-                    next_bit->ForceTimeQuanta(1, BitValue::Recessive);
+            next_bit->ForceTimeQuanta(1, BitValue::Recessive);
 
-                    driver_bit_frm->Print(true);
-                    monitor_bit_frm->Print(true);
+            driver_bit_frm->Print(true);
+            monitor_bit_frm->Print(true);
 
-                    /***************************************************************************** 
-                     * Execute test
-                     *****************************************************************************/
-                    PushFramesToLowerTester(*driver_bit_frm, *monitor_bit_frm);
-                    StartDriverAndMonitor();
-                    dut_ifc->SendFrame(golden_frm.get());
-                    WaitForDriverAndMonitor();
-                    CheckLowerTesterResult();
-                }
-            }
-            return (int)FinishTest();
+            /***************************************************************************** 
+             * Execute test
+             *****************************************************************************/
+            PushFramesToLowerTester(*driver_bit_frm, *monitor_bit_frm);
+            StartDriverAndMonitor();
+            dut_ifc->SendFrame(golden_frm.get());
+            WaitForDriverAndMonitor();
+            CheckLowerTesterResult();
+
+            return FinishElementaryTest();
         }
+
+        ENABLE_UNUSED_ARGS
 };
