4ab4520944149fa32f4930f25ce2aebd7b958ea2
Ondrej Ille <ondrej.ille@gmail.com>
2021-05-15 17:24:29 +0200

    Paralell builds by default.

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ mkdir -p build/Release
 # Debug build
 cd build/Debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../..
-make
+make -j `nproc`
 cd ../..
 cd /D
 # Release build
 cd build/Release
 cmake -DCMAKE_BUILD_TYPE=Release ../..
-make
+make -j `nproc`
 cd ../..
