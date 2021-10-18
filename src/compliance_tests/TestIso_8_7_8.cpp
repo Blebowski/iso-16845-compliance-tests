019f9faed1d7ba0ffe8cac2b0b802ffeb4adaa18
Ondrej Ille <ondrej.ille@tropicsquare.com>
2021-10-18 06:22:58 +0200

    compliance_tests: Flip to common elementary test definition per sample point.

--- src/compliance_tests/TestIso_8_7_8.cpp
+++ src/compliance_tests/TestIso_8_7_8.cpp
@@ -76,47 +76,31 @@ using namespace test_lib;
 class TestIso_8_7_8 : public test_lib::TestBase
 {
     public:
-        BitTiming test_nom_bit_timing;
 
         void ConfigureTest()
         {
             FillTestVariants(VariantMatchingType::Common);
-
-            // Elementary test for each possible positon of sample point, restrict to shortest
-            // possible PROP = 2, shortest possible PH2 = 1, PH1 always 0. Together we test
-            // TQ(N) - 3 tests!
-            for (size_t i = 1; i < nominal_bit_timing.GetBitLengthTimeQuanta() - 2; i++)
-                AddElemTest(TestVariant::Common, ElementaryTest(i + 1, FrameType::Can2_0));
+            AddElemTestForEachSamplePoint(TestVariant::Common, true, FrameType::Can2_0);
             
-            CanAgentMonitorSetTrigger(CanAgentMonitorTrigger::TxFalling);
-            CanAgentSetWaitForMonitor(true);
+            SetupMonitorTxTests();
         }
 
         int RunElemTest([[maybe_unused]] const ElementaryTest &elem_test,
                         [[maybe_unused]] const TestVariant &test_variant)
         {
-            // Calculate new bit-rate from configured one. Have same bit-rate, but
-            // different sample point. Shift sample point from 2 TQ up to 1 TQ before the
-            // end.
-            test_nom_bit_timing.brp_ = nominal_bit_timing.brp_;
-            test_nom_bit_timing.sjw_ = nominal_bit_timing.sjw_;
-            test_nom_bit_timing.ph1_ = 0;
-            test_nom_bit_timing.prop_ = elem_test.index_;
-            test_nom_bit_timing.ph2_ = nominal_bit_timing.GetBitLengthTimeQuanta() - elem_test.index_ - 1;
-            
-            /* Re-configure bit-timing for this test so that frames are generated with it! */
-            this->nominal_bit_timing = test_nom_bit_timing;
+            /* Generate test-specific bit timing with shifted sample point */
+            nominal_bit_timing = GenerateSamplePointForTest(elem_test, true);
 
             // Reconfigure DUT with new Bit time config with same bit-rate but other SP.
             dut_ifc->Disable();
-            dut_ifc->ConfigureBitTiming(test_nom_bit_timing, data_bit_timing);
+            dut_ifc->ConfigureBitTiming(nominal_bit_timing, data_bit_timing);
             dut_ifc->Enable();
             TestMessage("Waiting till DUT is error active!");
             while (this->dut_ifc->GetErrorState() != FaultConfinementState::ErrorActive)
                 usleep(100000);
 
             TestMessage("Nominal bit timing for this elementary test:");
-            test_nom_bit_timing.Print();
+            nominal_bit_timing.Print();
             
             uint8_t data_byte = 0x55;
             frame_flags = std::make_unique<FrameFlags>(FrameType::Can2_0, RtrFlag::DataFrame,
@@ -136,6 +120,10 @@ class TestIso_8_7_8 : public test_lib::TestBase
              *      Together with step 2, this achvies shortening by whole PH2 of received frame,
              *      but following bit length is effectively lengthened for IUT, so that IUT will
              *      not have remaining phase error to synchronize away during next bits.
+             *   4. Force last input delay of bit phase before PH2 of the same bit to dominant!
+             *      This will compensate input delay of IUT and make sure that IUT receives the
+             *      synchronization edge just in the first Time Quanta after Sample point and not
+             *      "input delay" later.
              *************************************************************************************/
             driver_bit_frm->GetBitOf(0, BitType::Ack)->bit_value_ = BitValue::Dominant;
 
@@ -147,6 +135,9 @@ class TestIso_8_7_8 : public test_lib::TestBase
             Bit *drv_bit = driver_bit_frm->GetBitOf(1, BitType::Data);
             for (size_t i = 0; i < drv_bit->GetPhaseLenTimeQuanta(BitPhase::Ph2); i++)
                 drv_bit->ForceTimeQuanta(i, BitPhase::Ph2, BitValue::Dominant);
+
+            // TODO: Compensate here by forcing last dut_input_delay cycles of phase before PH2
+            //       to dominant!
 
             driver_bit_frm->Print(true);
             monitor_bit_frm->Print(true);
