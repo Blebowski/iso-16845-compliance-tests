a0f881776d738ee0ff3f45f640a24ba4c0b9dd23
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2022-07-23 18:25:48 +0200

    license: Adjust for license equal to CTU CAN FD.

--- src/compliance_tests/TestIso_8_1_5.cpp
+++ src/compliance_tests/TestIso_8_1_5.cpp
@@ -1,9 +1,25 @@
 /****************************************************************************** 
  * 
- * @copyright Copyright (C) Ondrej Ille - All Rights Reserved
+ * ISO16845 Compliance tests 
+ * Copyright (C) 2021-present Ondrej Ille
  * 
- * Copying, publishing, distributing of this file is stricly prohibited unless
- * previously aggreed with author of this text.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this SW component and associated documentation files (the "Component"),
+ * to use, copy, modify, merge, publish, distribute the Component for
+ * educational, research, evaluation, self-interest purposes. Using the
+ * Component for commercial purposes is forbidden unless previously agreed with
+ * Copyright holder.
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Component.
+ * 
+ * THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHTHOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS
+ * IN THE COMPONENT.
  * 
  * @author Ondrej Ille, <ondrej.ille@gmail.com>
  * @date 12.7.2020
